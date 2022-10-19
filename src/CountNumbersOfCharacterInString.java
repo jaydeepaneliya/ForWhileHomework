436946c53b102bc077db6decff79c7233affb5df
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-19 18:16:57 +0530

    Search and count character 'a' commit

---
+++ src/CountNumbersOfCharacterInString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountNumbersOfCharacterInString {
+    public static void main(String[] args) {
+
+        System.out.println("Enter string here to count 'a' in it: ");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+
+        char search = 'a'; //Character to search 'a'
+
+        int count = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+
+            if(a.charAt(i) == search)
+            count++;
+
+        }
+
+        System.out.println("The character '"+search+"' appears in this string " + count + " times.");
+
+    }
+}
