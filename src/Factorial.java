3492de83baf673c6b5ad8635a4f5b26acd6b8369
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-19 15:54:30 +0530

    Factorial number commit

---
+++ src/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter any number fo which you want factorial: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 1;
+
+        //for loop created to get factorial of given number
+        for (int i = 1; i <= a ; i++) {
+            b*=i; //b = b * i
+
+
+        }
+        System.out.println(b);
+    }
+}
