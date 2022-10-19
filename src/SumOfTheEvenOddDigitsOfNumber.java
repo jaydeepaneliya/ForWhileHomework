830fd43ed086f4d7ab73ecdb84dbe6123811bfbb
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-19 21:50:19 +0530

    Sum of even and odd digit of the number commit

---
+++ src/SumOfTheEvenOddDigitsOfNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//I tried to run this program, but I could not get the required result.
+
+//public class SumOfTheEvenOddDigitsOfNumber {
+//    public static void main(String[] args) {
+//        System.out.println("Enter any 5-digit number to get sum of even and odd digits: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int evenSum = 0, oddSum = 0;
+//
+//        if (a <= 9999 || a >= 99999) {
+//            System.out.println("Entered number is not a valid number!!!");
+//        } else {
+//            for (int b = 0; a != 0; a=a/10) {
+//                a =a % 10;
+//                if (a % 2 == 0) {
+//                    evenSum = evenSum + a;
+//                } else {
+//                    oddSum = oddSum + a;
+//                }
+//
+//            }
+//            System.out.println("Sum of all the even digits of number: " + evenSum);
+//            System.out.println("Sum of all the odd digits of number: " + oddSum);
+//        }
+//
+//
+//    }
+//
+//}
+//
